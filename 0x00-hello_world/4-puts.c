ce89e7db3e490227e111f69ace2cbc95d7337cb9
James Moses <jamxsmosxs@gmail.com>
2023-10-06 09:06:17 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*Main Function*/
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzzle \n");
