888d1fafcd929639dad43af4e203c0fa6cca41ef
James Moses <jamxsmosxs@gmail.com>
2023-10-06 08:11:40 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+/*Main Function*/
 int main(void)
 {
-char myStr[] = "Programming is like building a multilingual puzzle \n";
+char myStr[] = '"\Programming is like building a multilingual puzzle \n';
 
 puts(myStr);
 
