48f4afcd996c9b18e4c661e62b3eada213af3ea6
James Moses <jamxsmosxs@gmail.com>
2023-10-06 21:37:32 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * Description: main - print a string to the standard error.
+ * return: a failure value (1)
+ */
 int main(void)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
