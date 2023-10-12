15d7cfe36ff3b5b9739751523dc9fb93924d5a3e
James Moses <jamxsmosxs@gmail.com>
2023-10-11 19:43:27 -0700

    new 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 
 /**
  * _islower - Checks for lowercase character
+ * @c: The character to be checked
  * Return: 1 for lowercase or 0 for anything else
  */
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
